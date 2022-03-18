5d151e89b51ecc7dfc85ac1d2853c7e85289cc50
Shota-G-Yoshida <xiangtaij660@gmail.com>
2022-03-18 11:18:40 +0900

    ブログ投稿の削除

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -9,6 +9,12 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="POST">
+            @csrf
+            @method('delete')
+            <input type='submit' style='display:none'>
+            <p class='delete'>[<span onclick='return deletePost(this);'>delete</span>]</p>
+        </form>
         <h1 class="title">
             {{ $post->title }}
         </h1>
@@ -22,5 +28,13 @@
             <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e) {
+                'use strict';
+                if(confirm('本当に削除しますか')) {
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
